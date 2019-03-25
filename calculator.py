06cd852c03f9b7046eea5cc6aadae460bef17555
Carson Otto <carsonhockey15@gmail.com>
2019-03-25 16:44:11 -0500

    Now asks users their desired tip amount and manaul application of sales tax value

--- calculator.py
+++ calculator.py
@@ -1,15 +1,17 @@
-#fixed tab ammount
+#Asks user how much their meal cost before tip/tax
 amount = raw_input("What's your total before tip/tax?")
 
-# sales tax, fixed to 7%
-tax = .07
+#Asks users the amount of sales tax applied to their meal
+tax = raw_input("What is the sales tax applied to your meal? (Enter as whole number)")
 
-# suggested tip percentage fixed to 15%
-tip = 0.15
+#Asks user what percent gratuity they would like to apply
+tip = raw_input("What percent tip would you like to apply? (Enter as whole number)")
 
-#total is the amount with tax and tip included
-total = (int(amount) + (int(amount) * tax) + (int(amount) * tip))
+#total is the amount with tax and tip included. We need to change every input into an integer to apply arithmetic. We also need to multiply the tax and tip by 0.01 because users input 
+#these values as whole numbers rather than percentages.git 
+total = int(amount) + (int(amount) * (int(tax) *.01)) + (int(amount) * (int(tip) * .01))
 
 result = "Your suggested total is " + str(total)
 
-print result
+print result 
+
