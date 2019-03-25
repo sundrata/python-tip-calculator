4bb93461dbdbee510202f474c3bc3eff95874c8c
Carson Otto <carsonhockey15@gmail.com>
2019-03-25 16:25:06 -0500

    Created a raw_input for initial total, allowing users to input their meal's price

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 #fixed tab ammount
-amount = 10
+amount = raw_input("What's your total before tip/tax?")
 
 # sales tax, fixed to 7%
 tax = .07
@@ -8,7 +8,7 @@ tax = .07
 tip = 0.15
 
 #total is the amount with tax and tip included
-total = (amount + (amount * tax) + (amount * tip))
+total = (int(amount) + (int(amount) * tax) + (int(amount) * tip))
 
 result = "Your suggested total is " + str(total)
 
